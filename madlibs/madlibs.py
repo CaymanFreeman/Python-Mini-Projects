501e7e78866c6d12820f7e19984126dc2ea7c749
MIlly <coileraldo@proton.me>
2023-08-27 17:13:12 -0600

    Added restart-ability

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -12,95 +12,99 @@ Story Templates:
 [2] Animal Comedy (Short)
 [3] Quest for the Kingdom (Long)
 [4] An Unexpected Friend (Short)
-[5] Rival Gangsters (Short)
-''')
+[5] Rival Gangsters (Short)''')
 
-story = -1
 while True:
-    story = input("Choose a story (1 to 5): ")
-    if story.isdigit():
-        story = int(story)
-        if 1 <= story <= 5:
-            break
+    story = -1
+    while True:
+        story = input("\nChoose a story (1 to 5): ")
+        if story.isdigit():
+            story = int(story)
+            if 1 <= story <= 5:
+                break
+            else:
+                print("\nYou must choose a number from 1 to 5")
         else:
-            print("\nYou must choose a number from 1 to 5")
-    else:
-        print("\nInvalid input, please enter a whole positive number.")
+            print("\nInvalid input, please enter a whole positive number.")
 
 
-def switch(num):
-    if num == 1:
-        return "monster_in_the_woods"
-    elif num == 2:
-        return "animal_comedy"
-    elif num == 3:
-        return "quest_for_the_kingdom"
-    elif num == 4:
-        return "unexpected_friend"
-    elif num == 5:
-        return "rival_gangsters"
+    def switch(num):
+        if num == 1:
+            return "monster_in_the_woods"
+        elif num == 2:
+            return "animal_comedy"
+        elif num == 3:
+            return "quest_for_the_kingdom"
+        elif num == 4:
+            return "unexpected_friend"
+        elif num == 5:
+            return "rival_gangsters"
 
 
-print()
+    print()
 
-with open(switch(story) + ".txt", "r") as file:
-    story = file.read()
+    with open(switch(story) + ".txt", "r") as file:
+        story = file.read()
 
-words = set()
-start_of_word = -1
-target_start = "<"
-target_end = ">"
+    words = set()
+    start_of_word = -1
+    target_start = "<"
+    target_end = ">"
 
-for i, char in enumerate(story):
-    if char == target_start:
-        start_of_word = i
-    if char == target_end and start_of_word != -1:
-        word = story[start_of_word:i + 1]
-        words.add(word)
-        start_of_word = -1
+    for i, char in enumerate(story):
+        if char == target_start:
+            start_of_word = i
+        if char == target_end and start_of_word != -1:
+            word = story[start_of_word:i + 1]
+            words.add(word)
+            start_of_word = -1
 
-answers = {}
+    answers = {}
 
-for word in words:
-    word_formatted = re.sub(r'\d+', '', word).strip('<>')
+    for word in words:
+        word_formatted = re.sub(r'\d+', '', word).strip('<>')
 
-    if '/s' in word:
-        plurality = 'singular'
-        word_formatted = word_formatted.replace('/s', '')
-    elif '/p' in word:
-        plurality = 'plural'
-        word_formatted = word_formatted.replace('/p', '')
-    else:
-        plurality = ''
+        if '/s' in word:
+            plurality = 'singular'
+            word_formatted = word_formatted.replace('/s', '')
+        elif '/p' in word:
+            plurality = 'plural'
+            word_formatted = word_formatted.replace('/p', '')
+        else:
+            plurality = ''
 
-    if '/ing' in word:
-        tense = '(ing)'
-        word_formatted = word_formatted.replace('/ing', '')
-    elif '/ed' in word:
-        tense = '(ed)'
-        word_formatted = word_formatted.replace('/ed', '')
-    else:
-        tense = ''
+        if '/ing' in word:
+            tense = '(ing)'
+            word_formatted = word_formatted.replace('/ing', '')
+        elif '/ed' in word:
+            tense = '(ed)'
+            word_formatted = word_formatted.replace('/ed', '')
+        else:
+            tense = ''
 
-    if word_formatted[0].lower() in 'aeiou':
-        article = 'an'
-    else:
-        article = 'a'
+        if word_formatted[0].lower() in 'aeiou':
+            article = 'an'
+        else:
+            article = 'a'
 
-    if plurality and tense:
-        article = 'a'
-        prompt = f"Enter {article} {plurality} {word_formatted} {tense}: "
-    elif plurality and not tense:
-        article = 'a'
-        prompt = f"Enter {article} {plurality} {word_formatted}: "
-    elif tense and not plurality:
-        prompt = f"Enter {article} {word_formatted} {tense}: "
-    else:
-        prompt = f"Enter {article} {word_formatted}: "
-    answer = input(prompt)
-    answers[word] = answer
+        if plurality and tense:
+            article = 'a'
+            prompt = f"Enter {article} {plurality} {word_formatted} {tense}: "
+        elif plurality and not tense:
+            article = 'a'
+            prompt = f"Enter {article} {plurality} {word_formatted}: "
+        elif tense and not plurality:
+            prompt = f"Enter {article} {word_formatted} {tense}: "
+        else:
+            prompt = f"Enter {article} {word_formatted}: "
+        answer = input(prompt)
+        answers[word] = answer
 
-for word in words:
-    story = story.replace(word, answers[word])
+    for word in words:
+        story = story.replace(word, answers[word])
 
-print("\nHere is your story:\n\n" + story)
+    print("\nHere is your story:\n\n" + story)
+
+    restart = input("\nWould you like to try another story (y/n)?: ")
+    if restart.lower() != 'y':
+        break
--- pig_game/pig_game.py
+++ pig_game/pig_game.py
@@ -12,60 +12,65 @@ The first player to score the agreed upon score (traditionally 100) or above win
 ''')
 
 while True:
-    players = input("Enter the number of players (2 to 4): ")
-    if players.isdigit():
-        players = int(players)
-        if 2 <= players <= 4:
-            break
-        else:
-            print("\nThere must be 2 to 4 players.")
-    else:
-        print("\nInvalid input, please enter a whole positive number.")
-
-player_scores = [0 for _ in range(players)]
-
-while True:
-    winning_score = input("What score would you like to play to?: ")
-    if winning_score.isdigit():
-        winning_score = int(winning_score)
-        if 0 < winning_score <= 500:
-            break
-        else:
-            print("\nYou can only play to a score from 0 to 500.")
-    else:
-        print("\nInvalid input, please enter a whole positive number.")
-
-
-def roll_die():
-    print("\nRolling die...")
-    min_value = 1
-    max_value = 6
-    num = random.randint(min_value, max_value)
-    sleep(0.5)
-    print(f"You've rolled a {num}.")
-    return num
-
-
-while max(player_scores) < winning_score:
-    for i in range(players):
-        print(f"\nCurrent turn: Player {i + 1}\nCurrent score: {player_scores[i]}")
-        turn_score = 0
-
-        while True:
-            should_roll = input("Would you like to roll the die (y/n)?: ")
-            if should_roll.lower() != "y":
-                break
-            rolled_value = roll_die()
-            if rolled_value == 1:
-                turn_score = 0
+    while True:
+        players = input("Enter the number of players (2 to 4): ")
+        if players.isdigit():
+            players = int(players)
+            if 2 <= players <= 4:
                 break
             else:
-                turn_score += rolled_value
-            print(f"\nYour earned score for this turn is: {turn_score}.")
+                print("\nThere must be 2 to 4 players.")
+        else:
+            print("\nInvalid input, please enter a whole positive number.")
 
-        player_scores[i] += turn_score
-        print(f"\nPlayer {i + 1}'s cumulative score for the game is: {player_scores[i]}.")
+    player_scores = [0 for _ in range(players)]
 
-max_score = max(player_scores)
-winning_player = player_scores.index(max_score)
-print(f"\nPlayer {winning_player + 1} is the winner with a score of {max_score}!")
+    while True:
+        winning_score = input("What score would you like to play to?: ")
+        if winning_score.isdigit():
+            winning_score = int(winning_score)
+            if 0 < winning_score <= 500:
+                break
+            else:
+                print("\nYou can only play to a score from 0 to 500.")
+        else:
+            print("\nInvalid input, please enter a whole positive number.")
+
+
+    def roll_die():
+        print("\nRolling die...")
+        min_value = 1
+        max_value = 6
+        num = random.randint(min_value, max_value)
+        sleep(0.5)
+        print(f"You've rolled a {num}.")
+        return num
+
+
+    while max(player_scores) < winning_score:
+        for i in range(players):
+            print(f"\nCurrent turn: Player {i + 1}\nCurrent score: {player_scores[i]}")
+            turn_score = 0
+
+            while True:
+                should_roll = input("Would you like to roll the die (y/n)?: ")
+                if should_roll.lower() != "y":
+                    break
+                rolled_value = roll_die()
+                if rolled_value == 1:
+                    turn_score = 0
+                    break
+                else:
+                    turn_score += rolled_value
+                print(f"\nYour earned score for this turn is: {turn_score}.")
+
+            player_scores[i] += turn_score
+            print(f"\nPlayer {i + 1}'s cumulative score for the game is: {player_scores[i]}.")
+
+    max_score = max(player_scores)
+    winning_player = player_scores.index(max_score)
+    print(f"\nPlayer {winning_player + 1} is the winner with a score of {max_score}!")
+    restart = input("\nWould you like to play again (y/n)?: ")
+
+    if restart != 'y':
+        break
